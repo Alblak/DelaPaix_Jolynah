cd740df7add716e386f68f36981f4f80de6f32c9
Gladkombi <gladkombigs@gmail.com>
2024-05-28 16:21:59 -0800

    [NEW] frist commit design

---
+++ views/aside.php
@@ -0,0 +1,34 @@
+<!-- Side-Nav-->
+<aside class="main-sidebar hidden-print ">
+            <section class="sidebar" id="sidebar-scroll">
+                
+                <!-- Sidebar Menu-->
+                <ul class="sidebar-menu">                                
+                    <li class="treeview">
+                        <a class="waves-effect waves-dark" href="Boutique.php">
+                            <i class="bi bi-house-door"></i><span> Chambres</span>
+                        </a>
+                    </li> 
+                    <li class="treeview">
+                        <a class="waves-effect waves-dark" href="Locataire.php">
+                            <i class="bi bi-people"></i><span> Locataire</span>
+                        </a>
+                    </li>
+                    <li class="treeview">
+                        <a class="waves-effect waves-dark" href="Affectation.php">
+                            <i class="bi bi-inboxes-fill"></i><span> Affectation</span>
+                        </a>
+                    </li>
+                    <li class="treeview">
+                        <a class="waves-effect waves-dark" href="paiement.php">
+                            <i class="bi bi-file-spreadsheet"></i><span> Paiement</span>
+                        </a>
+                    </li>
+                    <li class="treeview">
+                        <a class="waves-effect waves-dark" href="utilisateur.php">
+                            <i class="bi bi-person-square"></i><span> Utilisateurs</span>
+                        </a>
+                    </li>
+                </ul>
+            </section>
+        </aside>
